dedf319402eca3d738e0be9f6ddc6f0499ff3a86
Renato Oliveira <1140822@isep.ipp.pt>
2017-03-04 01:23:42 +0000

    implements ex07

---
+++ entrega1/ficha1/ex07/main.c
@@ -0,0 +1,47 @@
+/*
+ * Renato Oliveira > 1140822@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * main.c
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "printchar.h"
+
+
+/*
+ * Module 0 - Exercise 2
+ */
+
+int main(void) {
+	
+	int i;
+	pid_t pid;
+	pid_t pid_f;
+	for(i=0;i<2;i++)
+	{
+		pid=fork();
+		if(pid>0)
+		{
+			//pai
+			M("A");
+		}
+		else
+		{
+			//filho
+			M("B");
+			pid_f=fork();
+			if(pid_f>0)
+			{
+				
+				M("A");
+			    exit(1);
+			    //é necessário de acordo com o diagrama
+			}
+		}
+	}
+	return 0;
+}
---
+++ entrega1/ficha1/ex07/printchar.c
@@ -0,0 +1,19 @@
+/*
+ * Renato Oliveira > 1140822@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * printchar.c
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ * 
+ */
+void M(char * c)
+{
+	
+	printf("%s\n",c);
+}
