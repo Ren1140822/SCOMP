b4a0d5369a6cf5d3a5c5aaa668bffbc6ff35e00c
Daniel Gonçalves <1151452@isep.ipp.pt>
2017-03-06 23:39:51 +0000

    corrects exercise 06 [PL01-EX06]

---
+++ entrega1/ficha1/ex06/a/main.c
@@ -0,0 +1,73 @@
+/*
+ * Daniel Goncalves > 1151452@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * main.c
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
+#include "print_vec.h"
+
+#define SIZE 100000
+#define MAX_INTEGER 99
+
+/*
+ * PL 1 - Exercise 6
+ */
+int main(void)
+{
+	
+	int vec[SIZE], resultado[SIZE];
+	// Intializes random number generator
+	time_t t;
+	srand((unsigned) time(&t));
+	// Random values
+	int i;
+	for (i = 0; i < SIZE; i++)
+	{
+		vec[i] = 1;//rand() % MAX_INTEGER;
+	}
+	
+	printf("VEC:\n");
+	print_vec(vec, SIZE);
+	
+	pid_t pid;
+	
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error creating the child process.\n");
+		exit(-1);
+	}
+	
+	if (pid > 0)
+	{
+		for (i = SIZE / 2; i < SIZE; i++)
+		{
+			resultado[i] = vec[i]*4 + 20;
+		}
+
+		// wait for child and then print vec
+		wait(NULL);
+		// Print second half
+		print_vec((resultado + SIZE/2), SIZE/2);
+	}
+	else
+	{
+		for (i = 0; i < SIZE / 2; i++)
+		{
+			resultado[i] = vec[i]*4 + 20;
+		}
+		// Print first half
+		printf("RESULTADOS:\n");
+		print_vec(resultado, SIZE/2);
+		exit(1);
+	}
+
+	return 0;
+}
