f4be78ef64ef5f2fc2f5a925e902a886f365a8be
Renato Oliveira <1140822@isep.ipp.pt>
2017-03-23 15:11:18 +0000

    ex 1-9 completed(forgot to push changes earlier)clear

--- entrega1/ficha2/ex11/main.c
+++ entrega1/ficha2/ex11/main.c
@@ -13,23 +13,28 @@
 #include <sys/types.h>
 #include <string.h>
 #include "create_childs.h"
+#include "concat_string_with_int.h"
 
-#define  BUFFER_SIZE 81
-const int READ = 0;
-const int WRITE =1;
-const int NUMERO_FILHOS = 3;
+
+#define BUFFER_SIZE 81
+
+const int READ=0;
+const int WRITE=1;
+const int NUMERO_FILHOS =3;
+const int NUMERO_PRODUTOS=5;
+
 struct MENSAGEM_PEDIDO{
 	int id_filho;
 	int codigo_barras;
 } mensagem_pedido;
+
 struct MENSAGEM_RESPOSTA{
 	int codigo_barras;
 	float preco;
 	char descricao[BUFFER_SIZE];
 } mensagem_resposta;
 	
-// const int PAI_FILHO 0;
-// const int FILHO_PAI 1;
+
 
 /*
  * PL 2 - Exercise 11
@@ -58,16 +63,37 @@ int main(void)
 	  index_processo = create_childs(NUMERO_FILHOS);
 	 if (index_processo ==  0) 	// PROCESSO PAI
 	{
-		
+		int i;
+		struct MENSAGEM_RESPOSTA produtos[NUMERO_PRODUTOS];
+		for(i=0;i<NUMERO_PRODUTOS;i++)
+		{
+			produtos[i].codigo_barras=00000+i;
+			char * id_prod="Produto ";
+			char * full_description = concat_string_with_int(id_prod,i+1);
+			strncpy(produtos[i].descricao,full_description,BUFFER_SIZE);
+			
+			produtos[i].preco=0.1f;
+			
+		}
 		close(pipe_principal[WRITE]);
 		struct MENSAGEM_PEDIDO mensagem_pedido_temp;
 		while(read(pipe_principal[READ], &mensagem_pedido_temp, sizeof(mensagem_pedido)))
 		{ 
 	     	printf("filho %d c mensagem %d\n",mensagem_pedido_temp.id_filho,mensagem_pedido_temp.codigo_barras);
+	     	
+			//PESQUISAR ARRAY
+			for(i=0;i<NUMERO_PRODUTOS;i++)
+			{
+				if(mensagem_pedido_temp.codigo_barras == produtos[i].codigo_barras)
+				{
+					write(pipes_filhos[mensagem_pedido_temp.id_filho][WRITE],&produtos[i],sizeof(produtos[i]));
+					//write(pipes_filhos[mensagem_pedido_temp.id_filho][WRITE],&produtos[i].codigo_barras,sizeof(int));
+					//write(pipes_filhos[mensagem_pedido_temp.id_filho][WRITE],&produtos[i].descricao,BUFFER_SIZE);
+					//write(pipes_filhos[mensagem_pedido_temp.id_filho][WRITE],&produtos[i].preco,sizeof(float));
+				}
+				
+			}
 	    }
-		
-		// write(fd[WRITE], &msg, size);
-		
 		close(pipe_principal[READ]);
 		
 		// Esperar que o filho acabe (BOA PRÁTICA)
@@ -79,26 +105,12 @@ int main(void)
 		close(pipes_filhos[index_processo-1][WRITE]);
 		close(pipe_principal[READ]);
 		struct MENSAGEM_PEDIDO mensagem_pedido1;
+		struct MENSAGEM_RESPOSTA resposta_obtida;
 		mensagem_pedido1.id_filho=index_processo;
-		mensagem_pedido1.codigo_barras=1010101;
+		mensagem_pedido1.codigo_barras=000002;
 		write(pipe_principal[WRITE], &mensagem_pedido1, sizeof(mensagem_pedido));
-		
-		/* Working with strings
-		 * while(read(fd[READ], &msg, BUFFER_SIZE))
-		 * {
-		 * 		do something
-		 * }
-		*/
-		
-		
-		// read(fd[READ], &msg, size); ||
-		/*
-		 * while(read(fd[READ], &msg, size))
-		 * {
-		 * 		do something
-		 * }
-		*/
-		
+		read(pipes_filhos[index_processo-1][READ],&resposta_obtida,sizeof(resposta_obtida));
+		printf("recebido %s\n",resposta_obtida.descricao);
 		close(pipes_filhos[index_processo-1][READ]);
 		close(pipe_principal[WRITE]);
 	}
