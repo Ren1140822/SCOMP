1deaeb9a1104bd37f9c3ff4a2456e4b9b4091011
Daniel Gonçalves <1151452@isep.ipp.pt>
2017-03-24 09:35:39 +0000

    adds exercise 12 [EX12-PL02]

---
+++ entrega1/ficha2/ex12/custom_write.c
@@ -0,0 +1,40 @@
+/*
+ * Daniel Goncalves > 1151452@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * custom_write.c
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*
+ * Write to pipe content of a file.
+ * 
+ * returns the file length.
+ */
+int write_to_pipe(int fd, const char *filename, const unsigned int buffer_size)
+{
+	// Intialize buffer
+	char buffer[buffer_size];
+	// Open txt file
+	FILE *ptr_file = fopen(filename, "r");
+	// if open was unsuccessful
+	if (!ptr_file)
+	{
+		return 0;
+	}
+	// Iterate file lines until EOF
+	while (fgets(buffer, buffer_size, ptr_file))
+	{
+		// write to pipe
+		write(fd, buffer, strlen(buffer));
+	}
+	
+	// Close file
+	fclose(ptr_file);
+	
+	return 1;
+}
