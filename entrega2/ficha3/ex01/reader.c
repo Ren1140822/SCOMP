a48736bf18c6bb6e01e7fb4c9ecd4402437d8df2
Renato Oliveira <1140822@isep.ipp.pt>
2017-03-29 17:55:33 +0100

    adds exercise 01 [EX01-PL3-ENT02]

---
+++ entrega2/ficha3/ex01/reader.c
@@ -0,0 +1,87 @@
+/*
+ * Renato Oliveira > 1140822@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * reader.c
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+
+#include <string.h>
+#include "shd_type.h"
+#include "shm_helper.h"
+
+
+// Setting constants
+const int OPEN_TIMEOUT = 120;
+const int WRITE_TIMEOUT = 500;
+
+/*
+ * PL 3 - Exercise 00 - READER
+ */
+int main(int argc, char *argv[])
+{
+	// Vars
+	int fd, data_size = sizeof(shd_type);
+	shd_type *sh_data;
+	// Wait until writer finishes writing
+	printf("Waiting for writer to open shared memory . . .\n");
+	// Open shm
+	fd = shm_open_rd_mode(SHM_NAME, OPEN_TIMEOUT);
+	if (fd < 0)
+	{
+		perror("Open shared memory timed out.");
+		exit(EXIT_FAILURE);
+	}
+	// Truncate shm
+	if (ftruncate(fd, data_size) < 0)
+	{
+		perror("Truncating shared memory failed.");
+		exit(EXIT_FAILURE);
+	}
+	// Map shm
+	sh_data = (shd_type *) mmap(NULL, data_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (sh_data == MAP_FAILED)
+	{
+		perror("Mapping shared memory failed.");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Wait until writer finishes writing
+	printf("[ok]\n\nWaiting for writer to finish writing . . .\n");
+	if (wait_for_writer(&sh_data->w_flag, WRITE_TIMEOUT) < 0)
+	{
+		perror("Waiting for writer timed out.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	printf("Student number is: %d.\n",sh_data->student_number);
+	printf("Student name is: %s.\n",sh_data->student_name);
+	
+	
+	// Unmap & close
+	if (munmap(sh_data, data_size) < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if (close(fd) < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	// Unlink option
+	if ((argc > 1) && (strcmp("-rm", argv[1]) == 0))
+	{
+		if (shm_unlink(SHM_NAME) < 0)
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	return 0;
+}
---
+++ entrega2/ficha3/ex01/shd_type.h
@@ -0,0 +1,26 @@
+/*
+ * Renato Oliveira > 1140822@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * shd_type.h
+ */
+ 
+ 
+#ifndef SHD_TYPE_H
+#define SHD_TYPE_H
+
+#define SHD_VEC_SIZE 81
+#define SHM_NAME "/shm_f3_ex01"
+
+/*
+ * struct for a shared data type
+ */
+typedef struct
+{
+	int w_flag; // flag to check when finished writing (Set to 1 when written).
+	int student_number;
+	char student_name[SHD_VEC_SIZE];
+}
+shd_type;
+
+#endif
