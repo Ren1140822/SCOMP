b708032fa66d71cf3899b6af4c5d1a73b2b69798
Renato Oliveira <1140822@isep.ipp.pt>
2017-03-29 17:56:09 +0100

    adds exercise 03 [EX03-PL3-ENT02]

---
+++ entrega2/ficha3/ex03/shd_type.h
@@ -0,0 +1,24 @@
+/*
+ * Renato Oliveira > 1140822@isep.ipp.pt
+ * SCOMP - Turma 2DD
+ * 
+ * shd_type.h
+ */
+ 
+ 
+#ifndef SHD_TYPE_H
+#define SHD_TYPE_H
+
+
+#define SHM_NAME "/shm_f3_ex03"
+
+/*
+ * struct for a shared data type
+ */
+typedef struct
+{
+	int number; 
+}
+shd_type;
+
+#endif
